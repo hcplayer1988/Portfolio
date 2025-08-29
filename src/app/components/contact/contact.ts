ee1ea9ea6169be66d284afa789fcd167ef9c8c0a
Falko <162439151+hcplayer1988@users.noreply.github.com>
2025-08-29 12:54:02 +0200

    contact finished design

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss'
 })
 export class Contact {
 
+  http = inject(HttpClient);
+
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+
+  }
+
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php', // ennter my Domain later
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) { //  && !this.mailTest and th else if part only for testing can be deleted later!! Set the Mailttest to false!!!
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'), //console info can be deleted later
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
+    }
+  }
 }
