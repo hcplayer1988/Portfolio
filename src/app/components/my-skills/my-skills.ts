efaf3fdcd15a5809b0cb2a88b47413afd268ca69
Falko <162439151+hcplayer1988@users.noreply.github.com>
2025-09-01 15:32:22 +0200

    my skills @for

--- src/app/components/my-skills/my-skills.ts
+++ src/app/components/my-skills/my-skills.ts
@@ -1,11 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-my-skills',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-skills.html',
   styleUrl: './my-skills.scss'
 })
 export class MySkills {
 
+  skillList: {
+    img: string;
+    name: string;
+  }[] = [
+    {
+      img: "./imgs/icons/html.png",
+      name: "HTML"
+    },
+   {
+      img: "./imgs/icons/css.png",
+      name: "CSS"
+    },
+    {
+      img: "./imgs/icons/javaScript.png",
+      name: "JavaScript"
+    },
+    {
+      img: "./imgs/icons/angular.png",
+      name: "Angular"
+    },
+    {
+      img: "./imgs/icons/typeScript.png",
+      name: "TypeScript"
+    },
+  ]
 }
