f3b150d7a33577f906be577924b5655be5463050
Falko <162439151+hcplayer1988@users.noreply.github.com>
2025-09-11 07:08:13 +0200

    legal notice translation

--- src/app/imprint/imprint.ts
+++ src/app/imprint/imprint.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from '../shared/header/header';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-imprint',
-  imports: [Header],
+  imports: [Header, TranslateModule],
   templateUrl: './imprint.html',
   styleUrl: './imprint.scss'
 })
